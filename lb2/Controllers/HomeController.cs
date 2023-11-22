8419fe940521093aaf2a9a94630902870716ea06
Houdini <davletshin2004@mail.ru>
2023-11-22 20:20:32 +0300

    Initial commit

---
+++ lb2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using lb2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lb2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Title = "MyCafe";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult DrinkForm()
+        {
+            ViewBag.Title = "Drinks";
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult DrinkForm(DrinkData drinkData)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Title = "Bill";
+                ViewBag.Cost = drinkData.getPrice();
+                return View("Bill", drinkData);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ lb2/Models/DrinkData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lb2.Models
+{
+    public enum DrinkType
+    {
+        Tea,
+        Coffe,
+        Juice,
+        Alcohol
+    }
+    public class DrinkData
+    {
+        public DrinkType drinkType { get; set; }
+        [Required(ErrorMessage = "Пожалуйства, введите молоко")]
+        public int milk { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, введите сахар")]
+        public int sugar { get; set; }
+
+        public double getPrice()
+        {
+            double price = 0;
+            switch (drinkType)
+            {
+                case DrinkType.Tea:
+                    price += 10;
+                    break;
+                case DrinkType.Coffe:
+                    price += 15;
+                    break;
+                case DrinkType.Juice:
+                    price += 20;
+                    break;
+                case DrinkType.Alcohol:
+                    price += 30;
+                    break;
+                default:
+                    break;
+            }
+            price += 0.5 * milk;
+            price += 0.2 * sugar;
+            return price;
+        }
+    }
+}
