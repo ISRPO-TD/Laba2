857b4682187a1958df4ec5a842f68d37343eb202
Houdini <davletshin2004@mail.ru>
2023-11-23 12:05:14 +0300

    Films

---
+++ lb2/Controllers/FilmController.cs
@@ -0,0 +1,20 @@
+﻿using lb2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace lb2.Controllers
+{
+    public class FilmController : Controller
+    {
+        [HttpGet]
+        public ViewResult GetInfo(Models.FilmData filmData)
+        {
+            ViewBag.Title = "Film";
+            return View("Film", filmData);
+        }
+    }
+}
